3d2f8e7bcab596411b0bb48ad1f762026c67ed94
Mugiwara <37186888+ciss84@users.noreply.github.com>
2025-11-22 01:00:32 +0100

    Add files via upload

--- payload.js
+++ payload.js
@@ -1,44 +1,44 @@
 function load_gta(){
 LoadedMSG="Installer gta5 rdr2 prx Loaded !!!";
-PayloadLoader('payloads/installergta5rdr2prx.bin');
+LoadFromGHBLS('payloads/installergta5rdr2prx.bin');
 }
 
 function load_dumper(){
 LoadedMSG="Dumper v2 Loaded !!!";
-PayloadLoader('payloads/gamedumper.bin');
+LoadFromGHBLS('payloads/gamedumper.bin');
 }
 
 function load_ps4debug(){
 LoadedMSG="PS4Debug v1.0.15  Loaded !!!";
-PayloadLoader('payloads/ps4debug.bin');
+LoadFromGHBLS('payloads/ps4debug.bin');
 }
 
 function load_dbrestore(){
 LoadedMSG="restore DB Loaded !!!";
-PayloadLoader('payloads/restore.bin');
+LoadFromGHBLS('payloads/restore.bin');
 }
 
 function load_dbbackup(){
 LoadedMSG="Backup DB Loaded !!!";
-PayloadLoader('payloads/backup.bin');
+LoadFromGHBLS('payloads/backup.bin');
 }
 
 function load_pkg(){
 LoadedMSG="Backup Pkg v1.1 Loaded!!! Load your game!!!";
-PayloadLoader('payloads/pkgbackup.bin');
+LoadFromGHBLS('payloads/pkgbackup.bin');
 }
 
 function load_disabled_update(){
 LoadedMSG="Disabled Update Loaded!!!";
-PayloadLoader('payloads/disabled_update.bin');
+LoadFromGHBLS('payloads/disabled_update.bin');
 }
 
 function load_enabled_update(){
 LoadedMSG="Enabled Update Loaded!!!";
-PayloadLoader('payloads/enabled_update.bin');
+LoadFromGHBLS('payloads/enabled_update.bin');
 }
 
 function load_fix(){
 LoadedMSG="psfree fix Aio Loaded!!!";
-PayloadLoader('payloads/psfree-fix.bin');
+LoadFromGHBLS('payloads/psfree-fix.bin');
 }
